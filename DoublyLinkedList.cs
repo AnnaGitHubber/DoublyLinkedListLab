5f10507f6f4564d55910de44b3447f0496522585
Anna Kravchuk <annakravchuk880@gmail.com>
2025-04-27 21:40:46 +0300

    Add project files.

---
+++ DoublyLinkedList.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+public class DoublyLinkedList<T>
+{
+    private Node<T> head;
+    private Node<T> tail;
+
+    public void AddToEnd(T value)
+    {
+        Node<T> newNode = new Node<T>(value);
+        if (head == null)
+        {
+            head = tail = newNode;
+        }
+        else
+        {
+            tail.Next = newNode;
+            newNode.Previous = tail;
+            tail = newNode;
+        }
+    }
+
+
+    public void Remove(T data)
+    {
+        Node<T> current = head;
+        while (current != null)
+        {
+            if (current.Data.Equals(data))
+            {
+                if (current.Previous != null)
+                    current.Previous.Next = current.Next;
+                else
+                    head = current.Next;
+
+                if (current.Next != null)
+                    current.Next.Previous = current.Previous;
+                else
+                    tail = current.Previous;
+                break;
+            }
+            current = current.Next;
+        }
+    }
+
+    public Node<T> Find(T data)
+    {
+        Node<T> current = head;
+        while (current != null)
+        {
+            if (current.Data.Equals(data)) return current;
+            current = current.Next;
+        }
+        return null;
+    }
+
+    public void PrintList()
+    {
+        Node<T> current = head;
+        while (current != null)
+        {
+            Console.Write(current.Data + " ");
+            current = current.Next;
+        }
+        Console.WriteLine();
+    }
+
+    // Завдання 1: Вставити 66 після кожного від’ємного числа
+    public void Insert66AfterNegatives()
+    {
+        Node<T> current = head;
+        while (current != null)
+        {
+            if (current.Data is int val && val < 0)
+            {
+                Node<T> newNode = new Node<T>((T)Convert.ChangeType(66, typeof(T)));
+                newNode.Next = current.Next;
+                newNode.Previous = current;
+                if (current.Next != null)
+                    current.Next.Previous = newNode;
+                else
+                    tail = newNode;
+
+                current.Next = newNode;
+                current = newNode.Next;
+            }
+            else
+            {
+                current = current.Next;
+            }
+        }
+    }
+
+    // Завдання 2: Вставити перший від’ємний перед кожним числом == 20
+    public void InsertFirstNegativeBefore20()
+    {
+        Node<T> firstNegative = null;
+        Node<T> current = head;
+
+        while (current != null && firstNegative == null)
+        {
+            if (current.Data is double val && val < 0)
+                firstNegative = new Node<T>(current.Data);
+            current = current.Next;
+        }
+
+        if (firstNegative == null) return;
+
+        current = head;
+        while (current != null)
+        {
+            if (current.Data is double val && val == 20)
+            {
+                Node<T> newNode = new Node<T>(firstNegative.Data);
+                newNode.Next = current;
+                newNode.Previous = current.Previous;
+                if (current.Previous != null)
+                    current.Previous.Next = newNode;
+                else
+                    head = newNode;
+
+                current.Previous = newNode;
+                current = current.Next;
+            }
+            else
+            {
+                current = current.Next;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoublyLinkedListLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+            while (true)
+            {
+                Console.WriteLine("\nМеню:");
+                Console.WriteLine("1. Додати елемент до списку (цiлi)");
+                Console.WriteLine("2. Видалити елемент зi списку (цiлi)");
+                Console.WriteLine("3. Знайти елемент у списку (цiлi)");
+                Console.WriteLine("4. Вивести список (цiлi)");
+                Console.WriteLine("5. Завдання 1: Вставити 66 після вiд’ємних (цiлi)");
+                Console.WriteLine("6. Завдання 2: Вставити перший вiд’ємний перед 20 (дiйснi)");
+                Console.WriteLine("7. Додати елемент до списку (дiйснi)");
+                Console.WriteLine("8. Вивести список (дiйснi)");
+                Console.WriteLine("0. Вихiд");
+
+                Console.Write("Ваш вибiр: ");
+                string choice = Console.ReadLine();
+                // Створюємо список для цілих чисел
+                DoublyLinkedList<int> listInt = new DoublyLinkedList<int>();
+
+                // Створюємо список для дійсних чисел
+                DoublyLinkedList<double> listDouble = new DoublyLinkedList<double>();
+                // Заповнюємо список цілих чисел
+                listInt.AddToEnd(5);
+                listInt.AddToEnd(-3);
+                listInt.AddToEnd(7);
+                listInt.AddToEnd(-1);
+                listInt.AddToEnd(0);
+
+                // Заповнюємо список дійсних чисел
+                listDouble.AddToEnd(10.5);
+                listDouble.AddToEnd(-2.2);
+                listDouble.AddToEnd(20);
+                listDouble.AddToEnd(15);
+                listDouble.AddToEnd(20);
+
+                switch (choice)
+                {
+                    case "1":
+                        Console.Write("Введіть ціле число: ");
+                        int val = int.Parse(Console.ReadLine());
+                        listInt.AddToEnd(val);
+                        break;
+                    case "2":
+                        Console.Write("Введіть ціле число для видалення: ");
+                        int rem = int.Parse(Console.ReadLine());
+                        listInt.Remove(rem);
+                        break;
+                    case "3":
+                        Console.Write("Введіть ціле число для пошуку: ");
+                        int find = int.Parse(Console.ReadLine());
+                        var found = listInt.Find(find);
+                        Console.WriteLine(found != null ? "Знайдено." : "Не знайдено.");
+                        break;
+                    case "4":
+                        Console.Write("Список: ");
+                        listInt.PrintList();
+                        break;
+                    case "5":
+                        listInt.Insert66AfterNegatives();
+                        Console.WriteLine("Оновлений список:");
+                        listInt.PrintList();
+                        break;
+                    case "6":
+                        Console.WriteLine("Список до:");
+                        listDouble.PrintList();
+                        listDouble.InsertFirstNegativeBefore20();
+                        Console.WriteLine("Список після:");
+                        listDouble.PrintList();
+                        break;
+                    case "7":
+                        Console.Write("Введіть дійсне число: ");
+                        double d = double.Parse(Console.ReadLine());
+                        listDouble.AddToEnd(d);
+                        break;
+                    case "8":
+                        Console.Write("Список: ");
+                        listDouble.PrintList();
+                        break;
+                    case "0":
+                        return;
+                    default:
+                        Console.WriteLine("Невірна команда.");
+                        break;
+                }
+            }
+        }
+    }
+}
